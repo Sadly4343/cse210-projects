f026492ce892efd0d3f318b389b3f72eb92ad959
Sadly4343 <loganhartshorn@gmail.com>
2024-09-20 23:58:23 -0600

    commit

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -7,11 +7,11 @@ class Program
         Console.WriteLine("Hello Prep1 World!");
 
         Console.WriteLine("What is your first name?");
-        string fname = Console.ReadLine();
+        string firstName = Console.ReadLine();
         Console.WriteLine("What is your last name?");
-        string lname = Console.ReadLine();
+        string lastName = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {lname}, {lname} {fname}.");
+        Console.WriteLine($"Your name is {lastName}, {lastName} {firstName}.");
 
 
     }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,44 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        
+        Console.WriteLine("What is your grade?");
+        string gradeInText = Console.ReadLine();
+
+        int grade = int.Parse(gradeInText);
+        string letter = string.Empty;
+
+            if (grade >= 90)
+            {
+                letter = "A";
+            }
+            else if (grade >= 80)
+            {
+                letter = "B";
+            }
+            else if (grade >= 70)
+            {
+                letter = "C";
+            }
+            else if (grade >= 60)
+            {
+                letter = "D";
+            }
+            else
+            {
+                letter = "F";
+            }
+        Console.WriteLine($"Your grade for this class is: {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed this course!");
+        }
+        else
+        {
+            Console.WriteLine("Try again.");
+        }
+        
     }
 }
