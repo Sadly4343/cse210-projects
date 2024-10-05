cf0184cabaa5e9a932dcb6d7a2c818229ea35f69
Sadly4343 <loganhartshorn@gmail.com>
2024-10-04 23:27:57 -0600

    commit

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,28 @@
+public class Scripture
+{
+    private string _reference;
+
+    private List<Word> _words;
+
+    private Scripture(Reference reference, string text)
+    {
+
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+
+    }
+
+    public string GetDisplayText()
+    {
+        return "total";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return false;
+    }
+
+
+}
