aa744ab6cd7286d182547056cf9d77fade006569
Sadly4343 <loganhartshorn@gmail.com>
2024-10-12 18:43:40 -0600

    commit

--- foundation/Foundation2/Product.cs
+++ foundation/Foundation2/Product.cs
@@ -5,6 +5,13 @@ public class Product
     private int _price;
     private int _quantity;
 
+    public Product(string name, string productId, int price, int quantity)
+    {
+        _name = name;
+        _productId = productId;
+        _price = price;
+        _quantity = quantity;
+    }
     public int GetCost()
     {
         return _price * _quantity;
--- foundation/Foundation2/Program.cs
+++ foundation/Foundation2/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Foundation2 World!");
+        Product product1 = new Product("string bean", "1", 5, 2);
+        Product product2 = new Product("string beans", "2", 10, 4);
+
+        
+
+        Address a1 = new Address("random street", "otis orchards", "washington", "USA");
+
+        string addressText = a1.GetDisplayText();
+        Console.WriteLine(addressText);
+        Console.ReadLine();
+
     }
 }
+
